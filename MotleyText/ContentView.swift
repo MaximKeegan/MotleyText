4d6b45f05aabf16817d1837529f6dcab00f3752c
Maxim Keegan <maxim@keegan.ru>
2023-10-04 19:01:38 +0500

    First Commit

--- MotleyText/ContentView.swift
+++ MotleyText/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Double = 300
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                    .stroke(Color.red, lineWidth: 1)
+                    .frame(width: CGFloat(sliderValue), height: 300)
+                    .overlay(Group {
+                        Text("Марафон")
+                            .foregroundColor(Color.gray)
+                            .font(.system(size: 25)) +
+                        Text(" по SwiftUI")
+                            .font(.system(size: 25))
+                             +
+                        Text(" «Отцовский пинок»")
+                            .foregroundColor(Color.blue)
+                            .font(.system(size: 30))
+                            
+                    })
+            Slider(
+                value: $sliderValue,
+                in: 100...300,
+                step: 1
+            )
         }
         .padding()
     }
