e70c3941eae997245e551524b342388b6d092ec2
Maxim Keegan <maxim@keegan.ru>
2023-10-04 17:12:30 +0500

    Initial Commit

---
+++ MotleyText/MotleyTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotleyTextApp.swift
+//  MotleyText
+//
+//  Created by Maxim Keegan on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MotleyTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
